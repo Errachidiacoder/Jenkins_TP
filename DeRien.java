aacc0f71985b8ec5fd9656d8702fbcb6a8c5f292
hafsa daanouni <hafsadaanouni@gmail.com>
2024-12-10 21:28:10 +0100

    modifeir code

--- DeRien.java
+++ DeRien.java
@@ -1,1 +1,5 @@
-"public class DeRien { public static void main(String[] args) { System.out.println(\"De rien!\"); } }" 
+public class DeRien {
+    public static void main(String[] args) {
+        System.out.println("De rien!");
+    }
+}
